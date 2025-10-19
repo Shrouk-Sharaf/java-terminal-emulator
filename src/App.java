2943db3e1c68498269dcea40e7ebed1659788b2c
aminamohy <AminaMohy279@gmail.com>
2025-10-19 21:15:20 +0300

    updated Terminal class

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 class Parser {
     String commandName;
@@ -46,6 +49,8 @@ class Terminal {
         if (args.length == 0) 
         {
             myDirectory = new File(System.getProperty("user.home"));
+            
+
         } 
         else 
         {
@@ -66,6 +71,8 @@ class Terminal {
             if (newDir!=null&&newDir.exists()&&newDir.isDirectory()) 
             {
                 myDirectory = newDir;
+                System.out.println("Changed directory to: " + myDirectory.getAbsolutePath());
+
             } 
             else 
             {
@@ -109,10 +116,103 @@ class Terminal {
         }
     }
 
+
+    public  String resolvePath(String arg) {
+    File file = new File(arg);
+    if (file.isAbsolute()) {
+        return file.getAbsolutePath(); 
+    } else {
+        File resolved = new File(myDirectory, arg);
+        return resolved.getAbsolutePath();
+    }
+}
+  public void touch(String arg){
+      String path = resolvePath(arg);
+       System.out.println("Creating file at: " + path);
+      File newFile = new File(path);
+      if(newFile.exists()){
+         System.out.println("File already exist in  "+path);
+      }
+      else {
+      try {
+        boolean created = newFile.createNewFile();
+        if (created) {
+            System.out.println("File created successfully!");
+        } else {
+            System.out.println("File already exists!");
+        }
+    } catch (IOException e) {
+        System.out.println("Error creating file: " + e.getMessage());
+    }
+}
+  }
+  public void rmdir(String arg){
+    
+    
+    if(arg.equals("*")){
+        File files[] = myDirectory.listFiles();
+        if(files != null){
+            for(File f : files){
+                if(f.isDirectory() && f.listFiles().length == 0){
+                    boolean deleted = f.delete();
+                    if(deleted){
+                        System.out.println("Deleted empty directory: " + f.getName());
+                    }
+                }
+            }
+        }
+    } 
+    else { 
+        String path = resolvePath(arg);
+        File dir = new File(path);
+        if (dir.exists() && dir.isDirectory()) {
+            File[] contents = dir.listFiles();
+            if (contents != null && contents.length == 0) {
+                boolean deleted = dir.delete();
+                if (deleted)
+                    System.out.println("Directory deleted: " + dir.getAbsolutePath());
+                else
+                    System.out.println("Could not delete directory.");
+            } else {
+                System.out.println("Directory is not empty.");
+            }
+        } else {
+            System.out.println("Directory not found.");
+        }
+    }
+}
+
+  public void rm(String arg) {
+    String path = resolvePath(arg);
+    File file = new File(path);
+
+
+    if (!file.exists()) {
+        System.out.println("File not found: " + path);
+        return;
+    }
+
+
+    if (file.isFile()) {
+        boolean deleted = file.delete();
+        if (deleted) {
+            System.out.println("File deleted successfully: " + path);
+        } else {
+            System.out.println("Failed to delete file: " + path);
+        }
+    } else {
+        System.out.println("Error: '" + path + "' is not a file.");
+    }
+}
+
+  
+    
     public void execute(Parser parser) 
     {
         String command = parser.getCommandName();
         String[] args = parser.getArgs();
+        System.setProperty("user.dir", myDirectory.getAbsolutePath());
+
 
         switch (command) 
         {
@@ -128,6 +228,15 @@ class Terminal {
             case "mkdir":
                 mkdir(args);
                 break;
+            case "touch":
+                touch(args[0]);
+                break;
+            case "rmdir":
+                rmdir(args[0]);
+                break;
+            case "rm":
+                 rm(args[0]);
+                 break;
             default:
                 System.out.println("Unknown command: " + command);
         }
--- src/Test.java
+++ src/Test.java
@@ -1,32 +1,32 @@
-public class Test {
-    public static void main(String[] args) {
-        Parser parser = new Parser();
+// public class Test {
+//     public static void main(String[] args) {
+//         Parser parser = new Parser();
         
-        String[] testInputs = {
-            "ls",
-            "cd Documents",
-            "cp file1.txt file2.txt", 
-            "mkdir folder1 folder2 folder3",
-            "   touch  myfile.txt   ",  // Multiple spaces
-            "",                         // Empty input
-            "   "                       // Only spaces
-        };
+//         String[] testInputs = {
+//             "ls",
+//             "cd Documents",
+//             "cp file1.txt file2.txt", 
+//             "mkdir folder1 folder2 folder3",
+//             "   touch  myfile.txt   ",  // Multiple spaces
+//             "",                         // Empty input
+//             "   "                       // Only spaces
+//         };
         
-        System.out.println("=== Parser Testing ===\n");
+//         System.out.println("=== Parser Testing ===\n");
         
-        for (String input : testInputs) {
-            System.out.println("Testing: '" + input + "'");
+//         for (String input : testInputs) {
+//             System.out.println("Testing: '" + input + "'");
             
-            boolean success = parser.parse(input);
+//             boolean success = parser.parse(input);
             
-            if (success) {
-                System.out.println("Command: " + parser.getCommandName());
-                System.out.println("Args: " + java.util.Arrays.toString(parser.getArgs()));
-            }
-            else {
-                System.out.println("Failed to parse");
-            }
-            System.out.println();
-        }
-    }
-}
+//             if (success) {
+//                 System.out.println("Command: " + parser.getCommandName());
+//                 System.out.println("Args: " + java.util.Arrays.toString(parser.getArgs()));
+//             }
+//             else {
+//                 System.out.println("Failed to parse");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
