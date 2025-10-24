4209137103a827b18382c141f7d9fecc2c2582e8
Shrouk-Sharaf <sayedahmedroka@gmail.com>
2025-10-24 12:06:24 +0300

    added important import statements

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
